94feb793940d72ebfbe04227aafd8cab5daa412d
mueezullah <mueezullahip8@gmail.com>
2026-02-10 03:07:26 +0500

    feat: add home, login and signup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <div>Auth App</div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 };
