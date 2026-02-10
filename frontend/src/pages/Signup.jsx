10468d4edf759c6c0caf619a01df682d5c06ab1b
mueezullah <mueezullahip8@gmail.com>
2026-02-11 01:32:23 +0500

    feat: add signup UI and integrate backend API

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { handleError, handleSuccess } from "../utils";
 
 const Signup = () => {
+  const [signupInfo, setSignupInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    const copySignupInfo = { ...signupInfo }; // <-- shallow copy of the state object to avoid mutating the original state directly
+    copySignupInfo[name] = value;
+    setSignupInfo(copySignupInfo);
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const { name, email, password } = signupInfo;
+    if (!name || !email || !password) {
+      return handleError("All fields required");
+    }
+    try {
+      const url = "http://localhost:8080/auth/signup";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupInfo),
+      });
+
+      const result = await response.json();
+      const { success, message, error } = result;
+
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+      } else if (error) {
+        handleError(error);
+      } else if (!success) {
+        handleError(message);
+      }
+    } catch (err) {
+      handleError(err);
+    }
+  };
+
   return (
-    <div>
-      <div>SignUp</div>
+    <div className="container">
+      <h1>SignUp</h1>
+      <form onSubmit={handleSignup} action="">
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            autoFocus
+            placeholder="Enter Name"
+            value={signupInfo.name}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            placeholder="Enter Your Email..."
+            value={signupInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            placeholder="Enter Password..."
+            value={signupInfo.password}
+          />
+        </div>
+        <button type="submit">Signup</button>
+        <span>
+          Already have an account ? <Link to="/login">Login</Link>
+        </span>
+      </form>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
