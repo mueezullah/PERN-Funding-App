025de5a771cb79047983a58fd916fb9992b75a9d
mueezullah <mueezullahip8@gmail.com>
2026-02-12 01:57:02 +0500

    feat: integrate backend API to fetch and display products on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { handleError, handleSuccess } from "../utils";
+import { ToastContainer } from "react-toastify";
 
 const Home = () => {
+  const [loggedInUser, setLoggedInUser] = useState("");
+  const [products, setProducts] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem("loggediInUser"));
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const url = "http://localhost:8080/products/prods";
+      const headers = {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      };
+      const response = await fetch(url, headers);
+      const result = await response.json();
+      console.log(result);
+      setProducts(result);
+    } catch (err) {
+      handleError(err);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("loggediInUser");
+    handleSuccess("User LoggedOut Successfully!");
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
+  };
+
   return (
     <div>
-      <div>Home</div>
+      HOME PAGE
+      <h1>User Name: {loggedInUser}</h1>
+      <br />
+      <br />
+      <div>
+        <h3>Demo Products</h3>
+
+        {products &&
+          products?.map((item, index) => (
+            <ul key={index}>
+              <span>
+                {item.name}: {item.price}
+              </span>
+            </ul>
+          ))}
+      </div>
+      <br />
+      <button onClick={handleLogout}>Logout</button>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
