7d03f8bac19575ae77b5c019f5c8b49974ac5d3e
mueezullah <mueezullahip8@gmail.com>
2026-02-12 01:01:28 +0500

    feat: add login UI and integrate backend API

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { handleError, handleSuccess } from "../utils";
 
 const Login = () => {
+  const [loginInfo, setLoinInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    const copyLogiInfo = { ...loginInfo }; // <-- shallow copy of the state object to avoid mutating the original state directly
+    copyLogiInfo[name] = value;
+    setLoinInfo(copyLogiInfo);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { email, password } = loginInfo;
+    if (!email || !password) {
+      return handleError("All fields required");
+    }
+    try {
+      const url = "http://localhost:8080/auth/login";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginInfo),
+      });
+
+      const result = await response.json();
+      const { success, message, jwtToken, name, error } = result;
+      localStorage.setItem("token", jwtToken);
+      localStorage.setItem("loggediInUser", name);
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/home");
+        }, 1000);
+      } else if (error) {
+        handleError(error);
+      } else if (!success) {
+        handleError(message);
+      }
+    } catch (err) {
+      handleError(err);
+    }
+  };
+
   return (
-    <div>
-      <div>Login</div>
+    <div className="container">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin} action="">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            placeholder="Enter Your Email..."
+            value={loginInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            placeholder="Enter Password..."
+            value={loginInfo.password}
+          />
+        </div>
+        <button type="submit">Login</button>
+        <span>
+          Don't have an account ? <Link to="/signup">Signup</Link>
+        </span>
+      </form>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
