d4c19d8334a800ddc7a5c3573fbfe0f9975d24cb
mueezullah <mueezullahip8@gmail.com>
2026-02-19 04:36:21 +0500

    Removed Demo Products details

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { handleError, handleSuccess } from "../utils";
 import { ToastContainer } from "react-toastify";
@@ -6,28 +6,6 @@ import { ToastContainer } from "react-toastify";
 const Home = () => {
   const navigate = useNavigate();
   const [loggedInUser] = useState(localStorage.getItem("loggediInUser"));
-  const [products, setProducts] = useState("");
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const url = "http://localhost:8080/products/prods";
-        const headers = {
-          headers: {
-            Authorization: localStorage.getItem("token"),
-          },
-        };
-        const response = await fetch(url, headers);
-        const result = await response.json();
-        console.log(result);
-        setProducts(result);
-      } catch (err) {
-        handleError(err);
-      }
-    };
-
-    fetchProducts();
-  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -42,21 +20,7 @@ const Home = () => {
     <div>
       HOME PAGE
       <h1>User Name: {loggedInUser}</h1>
-      <br />
-      <br />
-      <div>
-        <h3>Demo Products</h3>
 
-        {products &&
-          products?.map((item, index) => (
-            <ul key={index}>
-              <span>
-                {item.name}: {item.price}
-              </span>
-            </ul>
-          ))}
-      </div>
-      <br />
       <button onClick={handleLogout}>Logout</button>
       <ToastContainer></ToastContainer>
     </div>
