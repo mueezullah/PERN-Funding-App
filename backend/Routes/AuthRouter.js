ace1d12ca8dab22e18f7ce33548a52a087e1b6fc
mueezullah <mueezullahip8@gmail.com>
2026-02-21 02:18:09 +0500

    feat(admin): add protected GET /users route for fetching all users

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,13 +1,15 @@
-const { signup, login } = require("../Controllers/AuthController");
+const { signup, login, getAllUsers } = require("../Controllers/AuthController");
 const {
   signupValidation,
   loginValidation,
 } = require("../Middlewares/AuthValidation");
+const { ensureAuthenticated, ensureAdmin } = require("../Middlewares/Auth");
 
 const router = require("express").Router();
 
 router.post("/signup", signupValidation, signup);
 router.post("/login", loginValidation, login);
 // router.post("/refeshtoken", loginValidation, login);
+router.get("/users", ensureAuthenticated, ensureAdmin, getAllUsers);
 
 module.exports = router;
