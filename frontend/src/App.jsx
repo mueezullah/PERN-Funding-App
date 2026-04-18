cf0bda170315a282c96f4b414533aed8f55a68f4
mueezullah <mueezullahip8@gmail.com>
2026-04-19 03:07:09 +0500

    Misc: added react-toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import RefreshHandler from "./RefreshHandler";
 import AppRoutes from "./routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -10,6 +12,7 @@ const App = () => {
     <div className="App">
       <RefreshHandler setIsAuthenticated={setIsAuthenticated} setIsLoading={setIsLoading} />
       <AppRoutes isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} isLoading={isLoading} />
+      <ToastContainer />
     </div>
   );
 };
